a7afce9e94dc4066dbbbbec368a33748780a8d4c
dauphinaissam <dauphinaissa@my.easternct.edu>
2017-10-23 13:53:41 -0400

    Changes
    I have added the string name and inserted a scanner to take in the users name. I also changed the two outputs.

--- hello.java
+++ hello.java
@@ -1,12 +1,17 @@
 // a simple program to practice common Github commands 
-
+import java.util.Scanner;
 public class hello {
 
     public static void main(String [] args) {
+	String name = “”;
 
-        System.out.println("Hello Class!");
+	Scanner input = new Scanner(System.in);
 
-        System.out.println("Bye");
+        System.out.println(“Please enter your name“);
+
+	name = input.nextLine();
+
+        System.out.println(“Hello ” + name + “, my name is Sam.”);
 
     }
 }
